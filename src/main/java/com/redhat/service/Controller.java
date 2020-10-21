02f2a698d4516a47db7f5233563f982797d6dc38
Kyle Le <kle@kle.remote.csb>
2020-10-21 10:38:26 -0700

    Update testing

---
+++ src/main/java/com/redhat/service/Controller.java
@@ -0,0 +1,24 @@
+package com.redhat.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class Controller {
+
+	private final Producer producer;
+
+    @Autowired
+    Controller(Producer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+    }
+}
