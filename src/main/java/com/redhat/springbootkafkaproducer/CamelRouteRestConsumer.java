7bca9f9aa45c674cb49bd2faa6e452b13f382bdf
Kyle Le <kle@kle.remote.csb>
2021-01-18 13:14:17 -0800

    Enabled REST forwarding of message

--- src/main/java/com/redhat/springbootkafkaproducer/CamelRouteRestConsumer.java
+++ src/main/java/com/redhat/springbootkafkaproducer/CamelRouteRestConsumer.java
@@ -1,5 +1,6 @@
 package com.redhat.springbootkafkaproducer;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +10,10 @@ public class CamelRouteRestConsumer extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		from("kafka:rest-topic?brokers=my-cluster-kafka-bootstrap:9092&groupId=demo-consumer")
-		.log("Message received from rest-topic: ${body}");
+		.log("Message received from rest-topic: ${body}")
+		.setHeader(Exchange.HTTP_METHOD, simple("POST"))
+		.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+		.to("http://rest-route-amq.apps.cluster-f610.sandbox1234.opentlc.com/messages");
 		
 	}
 
